6cc8b07be80850ae77c6e202080e70f2c99ec85f
Brian Downs <brian.downs@gmail.com>
2025-02-17 08:47:18 -0700

    framework
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- test/main.c
+++ test/main.c
@@ -35,8 +35,8 @@ test_slice_append()
     int_slice_append(s1, 15);
     int_slice_append(s1, 1);
 
-    UTEST_ASSERT_EQUAL_SIZE_T(s1->len, 8);
-    UTEST_ASSERT_EQUAL_SIZE_T(s1->cap, 8);
+    CC_ASSERT_EQUAL_SIZE_T(s1->len, (size_t)8);
+    CC_ASSERT_EQUAL_SIZE_T(s1->cap, (size_t)8);
 
     int_slice_free(s1);
 
@@ -52,9 +52,9 @@ test_slice_get()
     int_slice_append(s1, 100);
     int_slice_append(s1, 7);
 
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 9);
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 1), 100);
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 2), 7);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 9);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 1), 100);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 2), 7);
 
     int_slice_free(s1);
 
@@ -70,9 +70,9 @@ test_slice_contains()
     int_slice_append(s1, 100);
     int_slice_append(s1, 7);
     
-    UTEST_ASSERT(int_slice_contains(s1, 9));
-    UTEST_ASSERT(int_slice_contains(s1, 100));
-    UTEST_ASSERT(int_slice_contains(s1, 7));
+    CC_ASSERT(int_slice_contains(s1, 9));
+    CC_ASSERT(int_slice_contains(s1, 100));
+    CC_ASSERT(int_slice_contains(s1, 7));
 
     int_slice_free(s1);
 
@@ -90,7 +90,7 @@ test_slice_delete()
     
     int_slice_delete(s1, 2);
 
-    UTEST_ASSERT_EQUAL_SIZE_T(s1->len, 1);
+    CC_ASSERT_EQUAL_SIZE_T(s1->len, 2);
 
     int_slice_free(s1);
 
@@ -105,7 +105,7 @@ test_slice_replace_by_index()
     int_slice_append(s1, 9);
     int_slice_replace_by_idx(s1, 0, 42);
 
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 42);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 42);
 
     int_slice_free(s1);
 
@@ -124,7 +124,7 @@ test_slice_count()
     int_slice_append(s1, 7);
 
     size_t count = int_slice_count(s1, 7);
-    UTEST_ASSERT_EQUAL_SIZE_T(count, 3);
+    CC_ASSERT_EQUAL_SIZE_T(count, 3);
 
     int_slice_free(s1);
 
@@ -141,9 +141,9 @@ test_slice_sort()
     int_slice_append(s1, 9);
 
     int_slice_sort(s1);
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 9);
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 1), 100);
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 2), 1000);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 9);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 1), 100);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 2), 1000);
 
     int_slice_free(s1);
 
@@ -163,8 +163,8 @@ test_slice_replace_by_val()
 
     int_slice_replace_by_val(s1, 100, 700, 3);
 
-    UTEST_ASSERT_EQUAL_SIZE_T(int_slice_count(s1, 700), 3);
-    UTEST_ASSERT_EQUAL_SIZE_T(int_slice_count(s1, 100), 1);
+    CC_ASSERT_EQUAL_SIZE_T(int_slice_count(s1, 700), 3);
+    CC_ASSERT_EQUAL_SIZE_T(int_slice_count(s1, 100), 1);
 
     int_slice_free(s1);
 
@@ -178,9 +178,9 @@ test_slice_repeat()
 
     int_slice_repeat(s1, 88, 10);
 
-    UTEST_ASSERT_EQUAL_INT(s1->len, 10);
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 88);
-    UTEST_ASSERT_EQUAL_INT(int_slice_get(s1, s1->len-1), 88);
+    CC_ASSERT_EQUAL_INT(s1->len, 10);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, 0), 88);
+    CC_ASSERT_EQUAL_INT(int_slice_get(s1, s1->len-1), 88);
 
     int_slice_free(s1);
 
@@ -193,7 +193,7 @@ test_slice_grow()
     int_slice_t *s1 = int_slice_new(3);
 
     int_slice_grow(s1, 2);
-    UTEST_ASSERT_EQUAL_SIZE_T(s1->cap, 5);
+    CC_ASSERT_EQUAL_SIZE_T(s1->cap, 5);
 
     int_slice_free(s1);
 
@@ -210,18 +210,18 @@ test_slice_concat()
     int_slice_append(s1, 7);
     int_slice_append(s1, 7);
 
-    UTEST_ASSERT_EQUAL_SIZE_T(s1->len, 3);
-    UTEST_ASSERT_EQUAL_SIZE_T(s1->cap, 3);
+    CC_ASSERT_EQUAL_SIZE_T(s1->len, 3);
+    CC_ASSERT_EQUAL_SIZE_T(s1->cap, 3);
 
     int_slice_append(s2, 9);
     int_slice_append(s2, 9);
     int_slice_append(s2, 9);
 
-    UTEST_ASSERT_EQUAL_SIZE_T(s2->len, 3);
-    UTEST_ASSERT_EQUAL_SIZE_T(s2->cap, 3);
+    CC_ASSERT_EQUAL_SIZE_T(s2->len, 3);
+    CC_ASSERT_EQUAL_SIZE_T(s2->cap, 3);
 
     size_t s1_len = int_slice_concat(s1, s2);
-    UTEST_ASSERT_EQUAL_SIZE_T(s1_len, 6);
+    CC_ASSERT_EQUAL_SIZE_T(s1_len, 5); // purposefully wrong... correct (6)
 
     int_slice_free(s1);
     int_slice_free(s2);
@@ -234,17 +234,17 @@ main(int argc, char **argv)
 {
     utest_init();
 
-    utest_run("append", test_slice_append);
-    utest_run("get", test_slice_get);
-    utest_run("contains", test_slice_contains);
-    utest_run("delete", test_slice_delete);
-    utest_run("replace by index", test_slice_replace_by_index);
-    utest_run("count", test_slice_count);
-    utest_run("sort", test_slice_sort);
-    utest_run("replace by value", test_slice_replace_by_val);
-    utest_run("repeat", test_slice_repeat);
-    utest_run("grow", test_slice_grow);
-    utest_run("concat", test_slice_concat);
+    CC_RUN("append", test_slice_append);
+    CC_RUN("get", test_slice_get);
+    CC_RUN("contains", test_slice_contains);
+    CC_RUN("delete", test_slice_delete);
+    CC_RUN("replace by index", test_slice_replace_by_index);
+    CC_RUN("count", test_slice_count);
+    CC_RUN("sort", test_slice_sort);
+    CC_RUN("replace by value", test_slice_replace_by_val);
+    CC_RUN("repeat", test_slice_repeat);
+    CC_RUN("grow", test_slice_grow);
+    CC_RUN("concat", test_slice_concat);
 
     utest_complete();
 
--- test/utest.c
+++ test/utest.c
@@ -4,36 +4,42 @@
 
 #include "utest.h"
 
-static utest_suite_t *test_suite = {0};
+#define GREEN "\x1B[32m"
+#define RED   "\x1B[31m"
+#define RESET "\033[0m"
+
+static size_t count = 0;
+static size_t passed = 0;
+static size_t failed = 0;
+static clock_t start = 0;
+static clock_t end = 0;
 
 void
 utest_init()
 {
-    test_suite = (utest_suite_t*)calloc(1, sizeof(utest_suite_t));
-
-    test_suite->start = clock(); \
-    printf("\nRunning tests: %s\n", __FILE__);
+    start = clock();
+    printf("Running test suite...\n");
 }
 
 bool
-utest_run(const char *name, utest_func_t func) {
-    test_suite->count++;
-
-    printf("    %-20s", name);
+utest_run(const char *name, utest_func_t func, const char *func_name) {
+    count++;
 
     clock_t test_start = clock();
     bool ret = func();
     clock_t test_end = clock();
 
     double time_spent = (double)(test_end - test_start) / CLOCKS_PER_SEC;
-    if (ret) {
-        test_suite->failed++;
-        printf(RED"%-8s"RESET"   %-2.3f/ms\n", "failed", (time_spent*1000));
+    if (ret == false) {
+        failed++;
+        printf("    %-28s%-2s:%-21d" RED "%-8s" RESET "   %-2.3f/ms\n",
+            name, func_name, 0, "failed", (time_spent*1000));
         return false;
     }
 
-    test_suite->passed++;
-    printf(GREEN "%-8s"RESET"   %-2.3f/ms\n", "passed", (time_spent*1000));
+    passed++;
+    printf("    %-28s%-28s" GREEN "%-8s" RESET "   %-2.3f/ms\n",
+         name, "", "passed", (time_spent*1000));
 
     return true;
 }
@@ -41,9 +47,8 @@ utest_run(const char *name, utest_func_t func) {
 void
 utest_complete()
 {
-    test_suite->end = clock();
-    double ts = (double)(test_suite->end - test_suite->start) / CLOCKS_PER_SEC; \
+    end = clock();
+    double ts = (double)(end - start) / CLOCKS_PER_SEC; \
     printf("\nTotal: %-4lu Passed: %-4lu Failed: %-4lu in  %-2.3f/ms\n", \
-         test_suite->count, test_suite->passed, test_suite->failed, (ts*1000));
-    free(test_suite);
+         count, passed, failed, (ts*1000));
 }
